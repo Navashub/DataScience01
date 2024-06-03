3bd667fd98706d7aca902f211159bcb39b795382
Navashub <navasmuller01@gmail.com>
2024-06-03 14:19:38 +0300

    More lessons

--- Learn/Day8/test8.py
+++ Learn/Day8/test8.py
@@ -1,19 +1,16 @@
 # Welcome message
 print("Welcome to Python Pizza Deliveries!")
 
-# Prompt user for pizza size
 size = input("What size pizza do you want? S, M, or L: ")
 
-# Prompt user for pepperoni
 add_pepperoni = input("Do you want pepperoni? Y or N: ")
 
-# Prompt user for extra cheese
 extra_cheese = input("Do you want extra cheese? Y or N: ")
 
 # Initialize the bill
 bill = 0
 
-# Calculate the base price based on pizza size
+# Calculate price based on pizza size
 if size == "S":
     bill += 15
 elif size == "M":
@@ -21,16 +18,15 @@ elif size == "M":
 elif size == "L":
     bill += 25
 
-# Add the cost of pepperoni if requested
+
 if add_pepperoni == "Y":
     if size == "S":
         bill += 2
     else:
         bill += 3
 
-# Add the cost of extra cheese if requested
+
 if extra_cheese == "Y":
     bill += 1
 
-# Print the final bill
 print(f"Your final bill is: ${bill}.")
